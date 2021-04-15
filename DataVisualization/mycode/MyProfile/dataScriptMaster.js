fe91376b3de788840db01866a55f6e734bf08424
Pham Giang <phamgiang@cpe-172-100-3-47.twcny.res.rr.com>
2021-04-15 13:11:45 +0200

    Modify index.html

---
+++ DataVisualization/mycode/MyProfile/dataScriptMaster.js
@@ -0,0 +1,325 @@
+var bachelorScript = 
+[
+    {
+      "Id": 1,
+      "Name": "Marxism - Leninism",
+      "Credit": 5,
+      "Field": "Philosophy ",
+      "Grade": 1
+    },
+    {
+      "Id": 2,
+      "Name": "English A1",
+      "Credit": 4,
+      "Field": "Language",
+      "Grade": 3
+    },
+    {
+      "Id": 3,
+      "Name": "English A2",
+      "Credit": 5,
+      "Field": "Language",
+      "Grade": 3
+    },
+    {
+      "Id": 4,
+      "Name": "English B1",
+      "Credit": 5,
+      "Field": "Language",
+      "Grade": 3
+    },
+    {
+      "Id": 5,
+      "Name": "Vietnam Communist Party",
+      "Credit": 3,
+      "Field": "Philosophy ",
+      "Grade": 2
+    },
+    {
+      "Id": 6,
+      "Name": "Advanced Programming",
+      "Credit": 4,
+      "Field": "IT-CS",
+      "Grade": 3
+    },
+    {
+      "Id": 7,
+      "Name": "English B2",
+      "Credit": 5,
+      "Field": "Language",
+      "Grade": 4
+    },
+    {
+      "Id": 8,
+      "Name": "English C1",
+      "Credit": 5,
+      "Field": "Language",
+      "Grade": 4
+    },
+    {
+      "Id": 9,
+      "Name": "Fundamental Informatics 1",
+      "Credit": 2,
+      "Field": "IT-CS",
+      "Grade": 3.5
+    },
+    {
+      "Id": 10,
+      "Name": "Fundamental Informatics 4",
+      "Credit": 3,
+      "Field": "IT-CS",
+      "Grade": 3.5
+    },
+    {
+      "Id": 11,
+      "Name": "Ho Chi Minh Ideology",
+      "Credit": 2,
+      "Field": "Philosophy ",
+      "Grade": 2
+    },
+    {
+      "Id": 12,
+      "Name": "Data Structures and Algorithms",
+      "Credit": 3,
+      "Field": "IT-CS",
+      "Grade": 3
+    },
+    {
+      "Id": 13,
+      "Name": "Mechanic And Thermodynamic",
+      "Credit": 3,
+      "Field": "Physics",
+      "Grade": 3
+    },
+    {
+      "Id": 14,
+      "Name": "Algebra",
+      "Credit": 4,
+      "Field": "Mathematics",
+      "Grade": 2.5
+    },
+    {
+      "Id": 15,
+      "Name": "Analysis 1",
+      "Credit": 5,
+      "Field": "Mathematics",
+      "Grade": 1.5
+    },
+    {
+      "Id": 16,
+      "Name": "Computer Architecture",
+      "Credit": 3,
+      "Field": "IT-CS",
+      "Grade": 3
+    },
+    {
+      "Id": 17,
+      "Name": "Object Oriented Programming",
+      "Credit": 3,
+      "Field": "IT-CS",
+      "Grade": 3
+    },
+    {
+      "Id": 18,
+      "Name": "General Logistics",
+      "Credit": 2,
+      "Field": "Philosophy ",
+      "Grade": 1
+    },
+    {
+      "Id": 19,
+      "Name": "Software Engineering",
+      "Credit": 3,
+      "Field": "IT-CS",
+      "Grade": 2.5
+    },
+    {
+      "Id": 20,
+      "Name": "Electrical/Optical",
+      "Credit": 3,
+      "Field": "Physics",
+      "Grade": 2
+    },
+    {
+      "Id": 21,
+      "Name": "Analysis 2",
+      "Credit": 5,
+      "Field": "Mathematics",
+      "Grade": 3
+    },
+    {
+      "Id": 22,
+      "Name": "Computer Network",
+      "Credit": 3,
+      "Field": "IT-CS",
+      "Grade": 3
+    },
+    {
+      "Id": 23,
+      "Name": "Operating Systems",
+      "Credit": 3,
+      "Field": "IT-CS",
+      "Grade": 3.5
+    },
+    {
+      "Id": 24,
+      "Name": "Numerical Analysis Methods",
+      "Credit": 2,
+      "Field": "Mathematics",
+      "Grade": 4
+    },
+    {
+      "Id": 25,
+      "Name": "Discrete Mathematics",
+      "Credit": 4,
+      "Field": "Mathematics",
+      "Grade": 2
+    },
+    {
+      "Id": 26,
+      "Name": "Database",
+      "Credit": 3,
+      "Field": "IT-CS",
+      "Grade": 3.7
+    },
+    {
+      "Id": 27,
+      "Name": "General Management Science",
+      "Credit": 2,
+      "Field": "Philosophy ",
+      "Grade": 3.7
+    },
+    {
+      "Id": 28,
+      "Name": "Information Theory",
+      "Credit": 3,
+      "Field": "IT-CS",
+      "Grade": 2.5
+    },
+    {
+      "Id": 29,
+      "Name": "Optimazation",
+      "Credit": 2,
+      "Field": "Mathematics",
+      "Grade": 4
+    },
+    {
+      "Id": 30,
+      "Name": "Artificial Intelligence",
+      "Credit": 3,
+      "Field": "IT-CS",
+      "Grade": 3.5
+    },
+    {
+      "Id": 31,
+      "Name": "Modern Physics",
+      "Credit": 2,
+      "Field": "Physics",
+      "Grade": 3
+    },
+    {
+      "Id": 32,
+      "Name": "Probability and Statistics",
+      "Credit": 3,
+      "Field": "Mathematics",
+      "Grade": 3.5
+    },
+    {
+      "Id": 33,
+      "Name": "Digital Signal Processing",
+      "Credit": 2,
+      "Field": "Other",
+      "Grade": 2.5
+    },
+    {
+      "Id": 34,
+      "Name": "Technology Workshop",
+      "Credit": 3,
+      "Field": "Other",
+      "Grade": 3.5
+    },
+    {
+      "Id": 35,
+      "Name": "Compiler",
+      "Credit": 3,
+      "Field": "IT-CS",
+      "Grade": 3
+    },
+    {
+      "Id": 36,
+      "Name": "Computer Graphics",
+      "Credit": 3,
+      "Field": "IT-CS",
+      "Grade": 4
+    },
+    {
+      "Id": 37,
+      "Name": "Machine Learning",
+      "Credit": 3,
+      "Field": "IT-CS",
+      "Grade": 3.5
+    },
+    {
+      "Id": 38,
+      "Name": "Object Oriented Analysis and Design",
+      "Credit": 3,
+      "Field": "IT-CS",
+      "Grade": 3.5
+    },
+    {
+      "Id": 39,
+      "Name": "Image Processing",
+      "Credit": 3,
+      "Field": "IT-CS",
+      "Grade": 4
+    },
+    {
+      "Id": 40,
+      "Name": "Network Security",
+      "Credit": 3,
+      "Field": "IT-CS",
+      "Grade": 3
+    },
+    {
+      "Id": 41,
+      "Name": "Advanced Topics in Computer Science",
+      "Credit": 3,
+      "Field": "IT-CS",
+      "Grade": 4
+    },
+    {
+      "Id": 42,
+      "Name": "Computer Vision",
+      "Credit": 3,
+      "Field": "IT-CS",
+      "Grade": 4
+    },
+    {
+      "Id": 43,
+      "Name": "Industrial Training",
+      "Credit": 3,
+      "Field": "IT-CS",
+      "Grade": 4
+    },
+    {
+      "Id": 44,
+      "Name": "Multimedia Communication",
+      "Credit": 3,
+      "Field": "IT-CS",
+      "Grade": 3.7
+    },
+    {
+      "Id": 45,
+      "Name": "Thesis",
+      "Credit": 10,
+      "Field": "IT-CS",
+      "Grade": 3.7
+    },
+    {
+      "Id": 46,
+      "Name": "Average",
+      "Credit": 152,
+      "Field": "Average",
+      "Grade": 3.09
+    }
+   ]
